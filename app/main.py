f3e2e32b8ae6b728fcb0dfa4c0813943f8dcf7c7
Julian Haudek <julian.haudek@gmail.com>
2026-04-04 19:39:25 +0200

    MVP (need manual saving of .mcbs)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-import config
+from pathlib import Path
+from datetime import datetime
 
-print(config.MIDAS_MAPI_KEY)
-print(config.MIDAS_BASE_URL)
+from experiment.experiment_config import ExperimentConfig
+from experiment.batch_generator import BatchGenerator
 
-config.validate_config()
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+def main():
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    output_path = PROJECT_ROOT / "output" / f"single_span_beam_{timestamp}.csv"
+
+    cfg = ExperimentConfig(
+        n_models=2,
+        output_csv_path=str(output_path),
+        results_to_save=[
+            "mid_deflection_dz",
+            "left_reaction_fz",
+            "right_reaction_fz",
+        ],
+    )
+
+    generator = BatchGenerator(cfg)
+    generator.run()
+
+if __name__ == "__main__":
+    main()
